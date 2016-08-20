3d088e8fde48303ba481650bd48d9ba1bd546073
Alexrfy <peihaoy@gmail.com>
2016-08-20 17:41:03 -0400

    Update messbot.py

--- messbot.py
+++ messbot.py
@@ -1,3 +1,4 @@
+from flask import Flask, request
 import json
 import logging
 import os
@@ -9,6 +10,63 @@ import redis
 from dfrotz import DFrotz
 import models
 import parser
+
+app = Flask(__name__)
+
+
+# This needs to be filled with the Page Access Token that will be provided
+# by the Facebook App that will be created.
+PAT = str(os.environ.get('FBKey',3))
+
+#verify
+@app.route('/', methods=['GET'])
+def handle_verification():
+  print "Handling Verification."
+  if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
+    print "Verification successful!"
+    return request.args.get('hub.challenge', '')
+  else:
+    print "Verification failed!"
+    return 'Error, wrong validation token'
+
+#messaging
+@app.route('/', methods=['POST'])
+def handle_messages():
+    print "Handling Messages"
+    payload = request.get_data()
+    
+    for sender, message in messaging_events(payload):
+        send_message(PAT, sender, message)
+        
+#Sorts messages
+def messaging_events(payload):
+  """Generate tuples of (sender_id, message_text) from the
+  provided payload.
+  """
+  data = json.loads(payload)
+  messaging_events = data["entry"][0]["messaging"]
+  for event in messaging_events:
+    if "message" in event and "text" in event["message"]:
+      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+    else:
+      yield event["sender"]["id"], "I can't echo this"
+
+
+#Send the message. Limited to 320 char
+def send_message(token, recipient, text):
+  """Send the message text to recipient with id recipient.
+  """
+
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": token},
+    data=json.dumps({
+      "recipient": {"id": recipient},
+      "message": {"text": text.decode('unicode_escape')}
+    }),
+    headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print r.text
+
 
 
 def cmd_default(bot, message, z5bot, chat):
@@ -189,3 +247,5 @@ if __name__ == '__main__':
       password=config['redis']['password'],
   )
   z5bot.add_redis(r)
+  
+  app.run()
